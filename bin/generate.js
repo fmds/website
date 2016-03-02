645c97e9cf165f95745f6eba5a59e0a12fc21fc8
Codeship CI <mcmanus.simon@gmail.com>
2016-03-02 10:47:44 +0000

    adding generate commands

--- bin/generate.js
+++ bin/generate.js
@@ -2,8 +2,21 @@
 var speclate = require('speclate');
 var spec = require('../spec');
 
-console.log(spec)
 speclate.generate(spec, function (error) {
+  if (error) {
+    console.log('Error generating site: ', error)
+  }
+})
+
+
+speclate.api(spec, function (error) {
+  if (error) {
+    console.log('Error generating API: ', error)
+  }
+})
+
+
+speclate.pageify(spec, function (error) {
   if (error) {
     console.log('Error generating site: ', error)
   }
